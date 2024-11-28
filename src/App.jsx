0e60e3c9ece4465f1df816c39ceb5911fc96371f
paros <paroscrangsha@gmail.com>
2024-11-28 18:35:00 +0600

    linke ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } 
 import Home from './pages/Home/Home';
 import Layout from './components/Layout';
 import About from './pages/About/About';
+import Feature from './reuse/Feature';
+import Features from './pages/Features/Features';
+import Blog from './pages/Blog/Blog';
+import Faq from './pages/Faq/Faq';
+import Pricing from './pages/Pricing/Pricing';
+import Contact from './pages/Contact/Contact';
 
 function App() {
   let multipage = createBrowserRouter(createRoutesFromElements(
     <Route element={<Layout/>}>
       <Route index element={<Home/>}></Route>
       <Route path='/about' element={<About/>}></Route>
+      <Route path='/features' element={<Features/>}></Route>
+      <Route path='/pricing' element={<Pricing/>}></Route>
+      <Route path='/faq' element={<Faq/>}></Route>
+      <Route path='/blog' element={<Blog/>}></Route>
+      <Route path='/contact' element={<Contact/>}></Route>
     </Route>
   ))
 
