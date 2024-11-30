2f98b98a7085e9a05e29243026a959660deb1d95
Paros Rangsha <paroscrangsha@gmail.com>
2024-11-30 06:13:49 +0000

    Blog edit

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import './blog.css'
+import { Col, Container, Row } from 'react-bootstrap'
+import blogban from '../../img/blogban.png'
+
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <>
+      <div className="blogban">
+        <Container>
+          <Row>
+            <Col lg={{span:8, offset:2}}>
+              <h2>A UX Case Study on Creating a Studious Environment for Students</h2>
+              <p id='subhead'><b>Andrew Jonson</b>, Posted on 27th January 2021</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <img src={blogban} alt="blog Banner" />
+            </Col>
+          </Row>
+          <Row>
+              <Col lg={{span:8, offset:2}}>
+              <p id='blogpara'>Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside.</p>
+              </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   )
 }
 
--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -1,31 +1,10 @@
 import React from 'react'
 import './faq.css'
-import { Col, Container, Row } from 'react-bootstrap'
-import blogban from '../../img/blogban.png'
 
 const Faq = () => {
   return (
     <>
-      <div className="faq">
-        <Container>
-          <Row>
-            <Col lg={{span:8, offset:2}}>
-              <h2>A UX Case Study on Creating a Studious Environment for Students</h2>
-              <p id='subhead'><b>Andrew Jonson</b>, Posted on 27th January 2021</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <img src={blogban} alt="blog Banner" />
-            </Col>
-          </Row>
-          <Row>
-              <Col lg={{span:8, offset:2}}>
-              <p id='blogpara'>Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside.</p>
-              </Col>
-          </Row>
-        </Container>
-      </div>
+      <h2>This is FAQ</h2>
     </>
   )
 }
